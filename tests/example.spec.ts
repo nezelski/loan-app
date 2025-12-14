5678136d6d4591b00edd1fcd428ba3a788c83d19
Roman Nezelski <nezelski@gmail.com>
2025-12-14 16:35:27 +0200

    cleared unused import

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import Home from "../page-objects/pages/Home";
 import LoanDecision from "../page-objects/pages/LoanDecision";
 
